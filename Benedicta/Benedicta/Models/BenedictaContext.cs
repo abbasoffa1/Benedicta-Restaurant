932cf657b1e23a6582ea9d986b023a724376ac6f
Gunay <gunayaa@code.edu.az>
2019-05-06 12:31:49 +0400

    Database create and Create Area

---
+++ Benedicta/Benedicta/Models/BenedictaContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Benedicta.Models
+{
+    public class BenedictaContext:DbContext
+    {
+        public BenedictaContext():base ("BenedictaContext")
+        {
+
+        }
+        DbSet<About> Abouts { get; set; }
+        DbSet<Contact> Contacts { get; set; }
+        DbSet<ContactForm> ContactForms { get; set; }
+        DbSet<Menu> Menus { get; set; }
+        DbSet<MenuUpdate> MenuUpdates { get; set; }
+        DbSet<News> News { get; set; }
+        DbSet<Property> Propertys { get; set; }
+        DbSet<Reservation> Reservations { get; set; }
+        DbSet<ReserveTable> ReserveTables { get; set; }
+        DbSet<Service> Services { get; set; }
+        DbSet<Setting> Settings { get; set; }
+        DbSet<Slider> Sliders { get; set; }
+        DbSet<Tea> Teas { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Welcome> Welcomes { get; set; }
+
+    }
+
+}
